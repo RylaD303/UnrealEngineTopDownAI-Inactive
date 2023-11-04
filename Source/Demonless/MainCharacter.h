8f8845724067c68fb4bf14a9b1d1b7fbad534d96
Alexander Makedonski <alemak2002@gmail.com>
2023-11-04 12:32:12 +0200

    Removed paperZD due to unmatching engine version.

---
+++ Source/Demonless/MainCharacter.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "MainCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEMONLESS_API AMainCharacter : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
