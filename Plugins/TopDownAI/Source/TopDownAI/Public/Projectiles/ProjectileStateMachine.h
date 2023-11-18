56f095e3f60039647ff50b1a01b9a2cf6a9da58b
Alexander Makedonski <alemak2002@gmail.com>
2023-11-18 15:22:35 +0200

    Added projectile states.

--- Plugins/TopDownAI/Source/TopDownAI/Public/Projectiles/ProjectileStateMachine.h
+++ Plugins/TopDownAI/Source/TopDownAI/Public/Projectiles/ProjectileStateMachine.h
@@ -4,8 +4,25 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Containers/Array.h"
 #include "ProjectileStateMachine.generated.h"
 
+
+UENUM(BlueprintType)
+enum class EProjectileMovementState : uint8
+{
+    PMS_Forward			UMETA(DisplayName = "Forward"),
+    PMS_Homing			UMETA(DisplayName = "Homing"),
+};
+
+
+UENUM(BlueprintType)
+enum class EProjectileBehaviourState : uint8
+{
+    PMS_Normal			    UMETA(DisplayName = "Normal"),
+    PMS_Repeating			UMETA(DisplayName = "Repeating"),
+    PMS_OnlyOne             UMETA(DisplayName = "Only one"),
+};
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class TOPDOWNAI_API UProjectileStateMachine : public UActorComponent
@@ -13,15 +30,26 @@ class TOPDOWNAI_API UProjectileStateMachine : public UActorComponent
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
 	UProjectileStateMachine();
 
+    UFUNCTION(BlueprintCallable, Category = "Projectile")
+    void SetProjectileState(EProjectileMovementState NewState);
+	
+private:
+
+    UPROPERTY(EditAnywhere, Category = "ProjectileState")
+    EProjectileMovementState MovementState;
+
+    UPROPERTY(EditAnywhere, Category = "ProjectileState")
+    TArray<EProjectileMovementState> MovementStates;
+
+    UPROPERTY(EditAnywhere, Category = "ProjectileState")
+    EProjectileBehaviourState BehaviourState;
+    
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
+public:
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 		
