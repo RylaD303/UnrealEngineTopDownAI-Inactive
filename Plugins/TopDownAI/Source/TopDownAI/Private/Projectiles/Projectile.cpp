8a163740915f74ee1fbbad824473dc56937ff0b8
Alexander Makedonski <alemak2002@gmail.com>
2023-11-22 20:34:58 +0200

    Added implementation for projectile.

--- Plugins/TopDownAI/Source/TopDownAI/Private/Projectiles/Projectile.cpp
+++ Plugins/TopDownAI/Source/TopDownAI/Private/Projectiles/Projectile.cpp
@@ -1,28 +1,51 @@
 
 
-
+// AProjectile.cpp
 #include "Projectiles/Projectile.h"
-
+#include "Components/SphereComponent.h"
 
 // Sets default values
 AProjectile::AProjectile()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Create a sphere component for collision
+	USphereComponent* CollisionComponent = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionComponent"));
+	RootComponent = CollisionComponent;
+	CollisionComponent->InitSphereRadius(20.0f); // Adjust the radius as needed
+
+	// Create a projectile movement component
+	MovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComponent"));
+	MovementComponent->SetUpdatedComponent(CollisionComponent);
+	MovementComponent->InitialSpeed = 2000.0f; // Adjust the initial speed as needed
+	MovementComponent->MaxSpeed = 2000.0f; // Adjust the max speed as needed
+	MovementComponent->bRotationFollowsVelocity = true;
+	MovementComponent->bShouldBounce = true;
+
+	Damage = 10.0;
 }
 
-// Called when the game starts or when spawned
 void AProjectile::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	OnActorHit.AddDynamic(this, &AProjectile::OnOverlap);
 }
 
-// Called every frame
 void AProjectile::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
+void AProjectile::OnOverlap(AActor* SelfActor, AActor* OtherActor, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (OtherActor && OtherActor != this)
+	{
+		
+		OtherActor->TakeDamage(Damage, FDamageEvent(), nullptr, this);
+		
+		// Later: alert the ProjectileStateHandler.
+		Destroy();
+	}
+}
--- Plugins/TopDownAI/Source/TopDownAI/Public/Projectiles/Projectile.h
+++ Plugins/TopDownAI/Source/TopDownAI/Public/Projectiles/Projectile.h
@@ -21,7 +21,7 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-
+	UFUNCTION()
 	void OnOverlap(UPrimitiveComponent* OverlappedComp, AActor* Other, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 	UPROPERTY(VisibleAnywhere, Category = Movement)
