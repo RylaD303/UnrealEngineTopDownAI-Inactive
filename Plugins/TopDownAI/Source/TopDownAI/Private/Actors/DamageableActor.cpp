c9ad6a92a3ef15fc2fe388b2f1e120c3b24d962d
Alexander Makedonski <alemak2002@gmail.com>
2023-11-27 20:54:15 +0200

    Added ADamageableActor Implementation.

--- Plugins/TopDownAI/Source/TopDownAI/Private/Actors/DamageableActor.cpp
+++ Plugins/TopDownAI/Source/TopDownAI/Private/Actors/DamageableActor.cpp
@@ -1,25 +1,29 @@
 #include "Actors/DamageableActor.h"
-
-
-// Sets default values
 ADamageableActor::ADamageableActor()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
+    PrimaryActorTick.bCanEverTick = true;
+    Health = 100.0f;
 }
 
 // Called when the game starts or when spawned
 void ADamageableActor::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
 }
 
 // Called every frame
 void ADamageableActor::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
-
+    Super::Tick(DeltaTime);
 }
 
+// Function to apply damage to the actor
+void ADamageableActor::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+    Health -= DamageAmount;
+
+    if (Health <= 0.0f)
+    {
+        // Activate death signal.
+    }
+}
