2103afd02b1e2f19bac0db798cc1a73e43a9cdab
Alexander Makedonski <alemak2002@gmail.com>
2023-12-06 23:41:53 +0200

    Added CastAttack.

--- Plugins/TopDownAI/Source/TopDownAI/Public/Attacks/CastAttack.h
+++ Plugins/TopDownAI/Source/TopDownAI/Public/Attacks/CastAttack.h
@@ -2,17 +2,47 @@
 
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
+#include "Components/ActorComponent.h"
 #include "CastAttack.generated.h"
 
-/**
- * 
- */
-UCLASS()
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnCastFinished);
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class TOPDOWNAI_API UCastAttack : public UObject
-{
-	GENERATED_BODY()
+    GENERATED_BODY()
+
+public:
+    UCastAttack();
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Projectile States")
+    TArray<TSubclassOf<class AProjectile>> ProjectileClasses;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Projectile States")
+    TArray<float> ProjectileDelays;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cast Configuration")
+    float CastTime;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cast Configuration")
+    FVector MainDirection;
+
+    UFUNCTION(BlueprintCallable)
+    void StartCast();
+
+    UPROPERTY(BlueprintAssignable, Category = "Cast Events")
+    FOnCastFinished OnCastFinished;
+
+protected:
+    virtual void BeginPlay() override;
+
+private:
+    int32 CurrentProjectileIndex;
 	
-	
-	
-	
+    AActor* OwningActor;
+
+    void SpawnProjectile();
+
+    void OnProjectileFired();
+
+    FTimerHandle CastTimerHandle;
 };
