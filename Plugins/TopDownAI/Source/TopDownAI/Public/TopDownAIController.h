faeff648cd880addd67d4cce8604e486646c1386
Alexander Makedonski <alemak2002@gmail.com>
2023-12-09 19:22:41 +0200

    Added TopDownAI implementation.

--- Plugins/TopDownAI/Source/TopDownAI/Public/TopDownAIController.h
+++ Plugins/TopDownAI/Source/TopDownAI/Public/TopDownAIController.h
@@ -12,9 +12,37 @@
 UCLASS()
 class TOPDOWNAI_API ATopDownAIController : public AAIController
 {
-	GENERATED_BODY()
-	
-	
-	
-	
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AI")
+    class UBehaviorTree* BehaviorTreeAsset;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AI Configuration")
+    float MovementSpeed;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AI Configuration")
+    float AttackRange;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AI Configuration")
+    float DetectionRadius;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AI Configuration")
+    TArray<TSubclassOf<class UCastAttack>> CastAttacks;
+
+    UFUNCTION(BlueprintCallable, Category = "AI")
+    void StartAI();
+
+    UFUNCTION(BlueprintCallable, Category = "AI")
+    void StopAI();
+
+    UFUNCTION(BlueprintCallable, Category = "AI")
+    void PerformRandomCastAttack();
+
+    virtual void BeginPlay() override;
+
+
+private:
+    bool bIsAIActive;
+
 };
