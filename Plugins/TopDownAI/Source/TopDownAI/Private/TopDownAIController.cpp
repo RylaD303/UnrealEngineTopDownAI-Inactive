79aac056fca052041fc98f03c924c66bc7b9661f
Alexander Makedonski <alemak2002@gmail.com>
2023-12-10 23:54:14 +0200

    Added TopDownAI behaviour.

--- Plugins/TopDownAI/Source/TopDownAI/Private/TopDownAIController.cpp
+++ Plugins/TopDownAI/Source/TopDownAI/Private/TopDownAIController.cpp
@@ -1,8 +1,41 @@
-
-
-
 #include "TopDownAIController.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+void ATopDownAIController::StartAI()
+{
+    bIsAIActive = true;
+    RunBehaviorTree(BehaviorTreeAsset);
+    GetCharacterMovement()->MaxWalkSpeed = MovementSpeed;
+}
 
+void ATopDownAIController::StopAI()
+{
+    bIsAIActive = false;
+    StopMovement();
+    StopBehaviorTree();
+}
 
+void ATopDownAIController::PerformRandomCastAttack()
+{
+    if (bIsAIActive && CastAttacks.Num() > 0)
+    {
+        int32 RandomIndex = FMath::RandRange(0, CastAttacks.Num() - 1);
+        UCastAttack* CastAttack = NewObject<UCastAttack>(this, CastAttacks[RandomIndex]);
+        
+        if (CastAttack)
+        {
+            CastAttack->ExecuteAttack();
 
+            StopMovement();
+        }
+    }
+}
+
+void ATopDownAIController::BeginPlay()
+{
+    Super::BeginPlay();
+    
+    StartAI();
+}
